9aba42f3a31cd9a1fb83379d3d8784f913443f63
Pavol Vanek <pavol.vanek@erni.sk>
2015-03-17 22:47:32 +0100

    Update pom and arquillian.xml

--- src/test/java/JPATestIT.java
+++ src/test/java/JPATestIT.java
@@ -1,12 +1,14 @@
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jpa.test.model.Address;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,19 +19,23 @@ import javax.persistence.Persistence;
  */
 @RunWith(Arquillian.class)
 public class JPATestIT {
+//	@PersistenceContext(synchronization = SynchronizationType.UNSYNCHRONIZED, type = PersistenceContextType.TRANSACTION)
+	@PersistenceContext
+	EntityManager em;
 
 	@Deployment
 	public static JavaArchive setup() {
 		JavaArchive archive = ShrinkWrap.create(JavaArchive.class)
 				.addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
-				.addClass(Address.class);
+				.addClass(Address.class)
+				.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
 		System.out.println(archive.toString(true));
 		return archive;
 	}
 
 	@Test
 	public void testAddress() {
-		Persistence.createEntityManagerFactory("jpa_test").createEntityManager();
-		Address address = new Address();
+		List list = em.createQuery("Select a from Address a").getResultList();
+//		Address address = new Address();
 	}
 }
